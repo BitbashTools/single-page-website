ecbdf07b4a9e45ebbffb641cc6503343cafb7e93
BitbashTools <bitbashtools@gmail.com>
2025-07-25 15:27:43 +0500

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,15 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css'; // If you have global styles
-// src/index.js or src/main.js
-import '@fortawesome/fontawesome-free/css/all.min.css'; // Ensure Tailwind's styles are applied
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
